9ad22b8f336b9a98ae99ddd722594660f182ab06
Frost <Bear-Frost@users.noreply.github.com>
2024-07-22 17:37:08 +0800

    Add functionality to the theme switcher
    - choosing a theme using a theme switcher will change the color and
      background color of the elements base on a chosen theme
    
    - save the theme using localStorage

--- calculator-app-main/main.js
+++ calculator-app-main/main.js
@@ -1,4 +1,20 @@
+import { setTheme } from "./scripts/theme.js";
+
 (function () {
+  const themeButtons = document.querySelectorAll(".theme-btn");
+
+  const root = window.document.documentElement;
+  const userSystemTheme = window.matchMedia("(prefers-color-scheme:dark)");
+  const initializeTheme = () => {
+    setTheme(localStorage.getItem("calculatorTheme"), userSystemTheme, root);
+    if(localStorage.getItem("calculatorTheme") !== null){
+      const currentTheme = localStorage.getItem("calculatorTheme");
+      const themeIndex = (currentTheme === "dark") ? 0 : (currentTheme === "light") ? 1 : 2;
+      themeButtons[themeIndex].checked = true;
+    }
+  }
+  initializeTheme();
+
   const calculatorButtons = document.querySelectorAll(".calculator-btn");
   const calculatorResult = document.getElementById("calculator-result");
 
@@ -34,7 +50,7 @@
   const calculateResult = () => {
     try {
       const result = eval(calculatorInput);
-      if(result === Infinity){
+      if (result === Infinity) {
         throw new Error("can't divide by zero");
       }
       calculatorInput = String(result);
@@ -52,7 +68,10 @@
   };
 
   const deleteItem = () => {
-    const newCalculatorInput = calculatorInput.slice(0,calculatorInput.length -1);
+    const newCalculatorInput = calculatorInput.slice(
+      0,
+      calculatorInput.length - 1
+    );
     calculatorInput = newCalculatorInput;
     renderCalculatorValue();
   };
@@ -71,4 +90,12 @@
     .getElementById("reset-btn")
     .addEventListener("click", resetCalculator);
   document.getElementById("delete-btn").addEventListener("click", deleteItem);
+
+  themeButtons.forEach((theme) => {
+    theme.addEventListener("click", (event) => {
+      const themeName = event.target.dataset.themeName;
+      localStorage.setItem("calculatorTheme", themeName);
+      setTheme(localStorage.getItem("calculatorTheme"), userSystemTheme, root);
+    });
+  });
 })();
